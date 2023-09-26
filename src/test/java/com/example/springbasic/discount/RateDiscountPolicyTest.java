accbbdf2bfae38dec54b581d3cf187e8d1b3edf0
enterfive <enterfive@gmail.com>
2023-09-26 15:53:21 +0900

    20230926 테스트코드 추가

---
+++ src/test/java/com/example/springbasic/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,42 @@
+package com.example.springbasic.discount;
+
+import com.example.springbasic.member.Grade;
+import com.example.springbasic.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000) ;
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할이 적용되지 않아야 한다")
+    void vip_x() {
+        // given
+        Member member = new Member(2L, "memberVIP", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000) ;
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+
+
+}
